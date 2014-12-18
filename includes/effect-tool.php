9f7355455b26261debc8c50d2da28593f4807a46
Bruno <xuguangzhi2003@126.com>
2014-12-18 17:55:40 +0800

    blank to tab

--- includes/effect-tool.php
+++ includes/effect-tool.php
@@ -12,7 +12,7 @@
 if ( !class_exists( 'Effect_Tool' ) ):
 class Effect_Tool {
 
-    public static function get_all_effects() {
+	public static function get_all_effects() {
 		$all_effects = get_option('lazyload_lightbox_all_effects');
 		if ($all_effects===FALSE) {
 			$all_effects = array(
@@ -22,14 +22,14 @@ class Effect_Tool {
 					'download_url' => 'http://xiaoxu125634.github.io/Images-Lazyload-and-Lightbox/data/slimbox2_2.05(v1.0).zip',
 					'description' => ''
 				)
-	    	);
+			);
 
 			update_option('lazyload_lightbox_all_effects', $all_effects);
 		}
-    	return $all_effects;
-    }
+		return $all_effects;
+	}
 
-    public static function update_all_effects($remote_data=null) {
+	public static function update_all_effects($remote_data=null) {
 		$all_effects = self::get_all_effects();
 		$effects = self::get_effects();
 
@@ -42,31 +42,31 @@ class Effect_Tool {
 		$refresh = 0;
 		$do_save = 0;
 
-    	foreach ($all_effects as &$a_effect) {
-    		foreach ($remote_data as &$r_effect) {
-    			if ($a_effect['name'] == $r_effect['name']) {
-    				if ($a_effect['package_version'] < $r_effect['package_version']) {
-    					$a_effect['package_version'] = $r_effect['package_version'];
-    					$a_effect['download_url'] = $r_effect['download_url'];
-    					$refresh = 1;
-    				}
-    				if (isset($r_effect['deprecated']) && !isset($a_effect['deprecated'])) {
-    					$a_effect['deprecated'] = 1;
-    					$refresh = 1;
-    				}
-    				if ($a_effect['description'] != $r_effect['description']) {
-    					$a_effect['description'] = $r_effect['description'];
-    					$do_save = 1;
-    				}
-    				$r_effect['compared'] = 1;
-    				break;
-    			}
-    		}
-    	}
+		foreach ($all_effects as &$a_effect) {
+			foreach ($remote_data as &$r_effect) {
+				if ($a_effect['name'] == $r_effect['name']) {
+					if ($a_effect['package_version'] < $r_effect['package_version']) {
+						$a_effect['package_version'] = $r_effect['package_version'];
+						$a_effect['download_url'] = $r_effect['download_url'];
+						$refresh = 1;
+					}
+					if (isset($r_effect['deprecated']) && !isset($a_effect['deprecated'])) {
+						$a_effect['deprecated'] = 1;
+						$refresh = 1;
+					}
+					if ($a_effect['description'] != $r_effect['description']) {
+						$a_effect['description'] = $r_effect['description'];
+						$do_save = 1;
+					}
+					$r_effect['compared'] = 1;
+					break;
+				}
+			}
+		}
 		foreach ($remote_data as $r_key=>$r_effect) {
 			if (empty($r_effect['compared']) && empty($r_effect['deprecated'])) {
 				$all_effects[$r_key] = $r_effect;
-    			$refresh = 1;
+				$refresh = 1;
 			}
 		}
 		if ($refresh || $do_save) {
@@ -82,9 +82,9 @@ class Effect_Tool {
 		$return['refresh'] = $refresh;
 
 		return $return;
-    }
+	}
 
-    public static function check_updates_for_effects() {
+	public static function check_updates_for_effects() {
 		$all_effects = self::get_all_effects();
 		$effects = self::get_effects();
 
@@ -136,14 +136,14 @@ class Effect_Tool {
 		}
 
 		return $all_effects;
-    }
+	}
 
-    public static function init_effects() {
-    	$available_effects = self::reload_effects();
+	public static function init_effects() {
+		$available_effects = self::reload_effects();
 		update_option('lazyload_lightbox_effects', $available_effects);
-    }
+	}
 
-    public static function get_effects() {
+	public static function get_effects() {
 		$need_reload = FALSE;
 		$need_handle = FALSE;
 		$need_save = FALSE;
@@ -172,10 +172,10 @@ class Effect_Tool {
 
 		// return available_effects
 		return $effects;
-    }
+	}
 
-    public static function reload_effects() {
-    	$available_effects = array();
+	public static function reload_effects() {
+		$available_effects = array();
 
 		$files = scandir(LAZYLOAD_LIGHTBOX_PLUGIN_EFFECTS_DIR);
 		if (!empty($files)) {
@@ -190,9 +190,9 @@ class Effect_Tool {
 		}
 
 		return $available_effects;
-    }
+	}
 
-    public static function handle_effects($effects) {
+	public static function handle_effects($effects) {
 		$effects_tmp = array();
 		foreach ($effects as $effect_name=>&$effect) {
 			if (empty($effect['adapters'])) {
@@ -230,7 +230,7 @@ class Effect_Tool {
 			$effects_tmp[$effect_name] = $effect;
 		}
 		return $effects_tmp;
-    }
+	}
 
 }
 endif;
